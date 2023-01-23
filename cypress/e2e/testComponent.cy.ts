b3576e93e50426bc6da142e3540bd89be114e7b2
Nuszpl Botond <nuszplbotond@gmail.com>
2023-01-23 19:53:04 +0100

    Fixing cypress test

--- cypress/e2e/testComponent.cy.ts
+++ cypress/e2e/testComponent.cy.ts
@@ -3,7 +3,7 @@ context('Test Componenet', () => {
   it('Should render a page with test and test2 text present on the screen', () => {
     cy.visit('http://localhost:3000')
 
-    cy.contains('Test3')
+    cy.contains('Test')
     cy.contains('Test2')
   })
 })
