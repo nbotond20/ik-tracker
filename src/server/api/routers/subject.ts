b53bd6ab1d6589d97b76ec456a0920f87a907841
Nuszpl Botond <nuszplbotond@gmail.com>
2023-02-03 16:57:51 +0100

    Added starting point for subject route

---
+++ src/server/api/routers/subject.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+import { createTRPCRouter, publicProcedure } from '../trpc'
+
+export const subjectRoute = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.subject.findMany()
+  }),
+
+  getById: publicProcedure.input(z.object({ id: z.string() })).query(({ ctx, input }) => {
+    return ctx.prisma.subject.findUnique({
+      where: {
+        id: input.id,
+      },
+    })
+  }),
+})
