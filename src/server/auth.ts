174441aa799bf951cfb16c125518504ae7ac9556
Nuszpl Botond <nuszplbotond@gmail.com>
2023-02-26 15:19:07 +0100

    Removing unsatble_ prefix

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,13 +1,10 @@
 import { authOptions } from '@pages/api/auth/[...nextauth]'
 import { type GetServerSidePropsContext } from 'next'
-import { unstable_getServerSession } from 'next-auth'
+import { getServerSession } from 'next-auth'
 
 /**
- * Wrapper for unstable_getServerSession, used in trpc createContext and the
+ * Wrapper for getServerSession, used in trpc createContext and the
  * restricted API route
- *
- * Don't worry too much about the "unstable", it's safe to use but the syntax
- * may change in future versions
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
@@ -16,5 +13,5 @@ export const getServerAuthSession = async (ctx: {
   req: GetServerSidePropsContext['req']
   res: GetServerSidePropsContext['res']
 }) => {
-  return await unstable_getServerSession(ctx.req, ctx.res, authOptions)
+  return await getServerSession(ctx.req, ctx.res, authOptions)
 }
