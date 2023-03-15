a2210b07350cb8407a565551ddac50f175ce4d7b
Nuszpl Botond <nuszplbotond@gmail.com>
2023-03-15 13:53:31 +0100

    SWC minified

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const config = {
   reactStrictMode: true,
   /* If trying out the experimental appDir, comment the i18n config out
    * @see https://github.com/vercel/next.js/issues/41980 */
+  swcMinify: true,
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
