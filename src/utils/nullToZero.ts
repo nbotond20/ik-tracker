37a69efb68526af40da8809e01e82a740c881b9b
Nuszpl Botond <nuszplbotond@gmail.com>
2023-04-04 18:35:18 +0200

    Fix safe number check

--- src/utils/nullToZero.ts
+++ src/utils/nullToZero.ts
@@ -1,4 +1,9 @@
 export const safeNumber = (value: number | null | undefined) => {
+  if (value === null) return 0
+  if (value === undefined) return 0
+  if (value === Infinity) return 0
+  if (value === -Infinity) return 0
+  if (typeof value !== 'number') return 0
   if (Number.isNaN(value)) return 0
   return value ?? 0
 }
