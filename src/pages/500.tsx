7c986cea022e312ab073a443fc610341a8a553f5
Nuszpl Botond <nuszplbotond@gmail.com>
2023-04-25 20:37:37 +0200

    Add retry to 500 page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -22,6 +22,12 @@ const InternalErrorPage: NextPage = () => {
             <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
               {t('internalError.description')}{' '}
             </p>
+            <button
+              onClick={() => window.location.reload()}
+              className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+            >
+              {t('internalError.tryAgainBtn')}
+            </button>
           </div>
         </div>
       </section>
