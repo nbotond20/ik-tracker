26cfa22befa59817c49b6d986f453ebf0f1fad97
Nuszpl Botond <nuszplbotond@gmail.com>
2023-05-10 22:41:11 +0200

    Installing microsoft clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { getThemeModeFromLocalStorage } from '@hooks/useThemeMode'
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   const themeMode = getThemeModeFromLocalStorage()
@@ -35,6 +36,18 @@ export default function Document() {
             <meta name="background-color" media="(prefers-color-scheme: dark)" content="#111827" />
           </>
         )}
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "h16dywmsom");`,
+          }}
+        />
       </Head>
       <body>
         <Main />
